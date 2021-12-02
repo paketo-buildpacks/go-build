ff3e38a9cfa01efa8edaa2e270786669a67897ee
Forest Eckhardt <feckhardt@pivotal.io>
2021-12-02 18:56:12 +0000

    Corrects logging order expectation

--- integration/default_test.go
+++ integration/default_test.go
@@ -258,10 +258,10 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 				MatchRegexp(`      Completed in ([0-9]*(\.[0-9]*)?[a-z]+)+`),
 				"",
 				"  Assigning launch processes:",
+				fmt.Sprintf("    workspace:                  /layers/%s/targets/bin/workspace",
+					strings.ReplaceAll(settings.Buildpack.ID, "/", "_")),
 				fmt.Sprintf("    reload-workspace (default): watchexec --restart --watch /workspace --watch /layers/%s/targets/bin '/layers/%s/targets/bin/workspace'",
 					strings.ReplaceAll(settings.Buildpack.ID, "/", "_"),
-					strings.ReplaceAll(settings.Buildpack.ID, "/", "_")),
-				fmt.Sprintf("    workspace:                  /layers/%s/targets/bin/workspace",
 					strings.ReplaceAll(settings.Buildpack.ID, "/", "_")),
 			))
 
